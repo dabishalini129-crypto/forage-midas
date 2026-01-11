0045c21597b32429c821f12b149b9e8c3060e3d7
Abishalini D <dabishalini129@gmail.com>
2026-01-11 22:19:06 +0530

    Completed Midas Core task

---
+++ src/main/java/com/jpmc/midascore/component/MidascoreFileLoader.java
@@ -0,0 +1,7 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
